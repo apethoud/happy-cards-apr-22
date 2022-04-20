0ad1880a2a610e9c04201abff8880c3efd36ce24
Andrew Pethoud <andrewpethoud@gmail.com>
2022-04-20 11:21:33 -0700

    add remaining form fields

--- happy-cards-apr-22/src/components/CardForm.js
+++ happy-cards-apr-22/src/components/CardForm.js
@@ -2,11 +2,17 @@ import { useState } from "react";
 
 export default function CardForm() {
   const [recipientName, setRecipientName] = useState("");
+  const [recipientEmail, setRecipientEmail] = useState("");
+  const [senderName, setSenderName] = useState("");
+  const [senderEmail, setSenderEmail] = useState("");
 
   function handleSubmit(event) {
     event.preventDefault();
     const payload = {
       recipientName,
+      recipientEmail,
+      senderName,
+      senderEmail,
     };
     console.log("payload is: ", payload);
   }
@@ -24,6 +30,42 @@ export default function CardForm() {
             type="text"
             value={recipientName}
             onChange={(event) => setRecipientName(event.target.value)}
+          />
+        </div>
+        <div className="Form-InputWrapper">
+          <label className="Label" htmlFor="Their Email">
+            Their Email
+          </label>
+          <input
+            className="Input"
+            name="Their Email"
+            type="text"
+            value={recipientEmail}
+            onChange={(event) => setRecipientEmail(event.target.value)}
+          />
+        </div>
+        <div className="Form-InputWrapper">
+          <label className="Label" htmlFor="Your Name">
+            Your Name
+          </label>
+          <input
+            className="Input"
+            name="Your Name"
+            type="text"
+            value={senderName}
+            onChange={(event) => setSenderName(event.target.value)}
+          />
+        </div>
+        <div className="Form-InputWrapper">
+          <label className="Label" htmlFor="Your Email">
+            Your Email
+          </label>
+          <input
+            className="Input"
+            name="Your Email"
+            type="text"
+            value={senderEmail}
+            onChange={(event) => setSenderEmail(event.target.value)}
           />
         </div>
         <button type="submit" className="Button">
