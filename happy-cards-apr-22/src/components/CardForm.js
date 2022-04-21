7fa6bc7d6f9e303d2011ea87b635f4c19f1bb259
Andrew Pethoud <andrewpethoud@gmail.com>
2022-04-20 19:05:10 -0700

    add uncontrolled input example and reusable component for inputs

--- happy-cards-apr-22/src/components/CardForm.js
+++ happy-cards-apr-22/src/components/CardForm.js
@@ -1,10 +1,12 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
+import LabeledTextInput from "./LabeledTextInput";
 
 export default function CardForm() {
   const [recipientName, setRecipientName] = useState("");
   const [recipientEmail, setRecipientEmail] = useState("");
   const [senderName, setSenderName] = useState("");
   const [senderEmail, setSenderEmail] = useState("");
+  const exampleRef = useRef();
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -15,64 +17,43 @@ export default function CardForm() {
       senderEmail,
     };
     console.log("payload is: ", payload);
+    console.log("exampleRef.current.value is: ", exampleRef.current.value);
   }
 
   return (
     <>
       <form onSubmit={handleSubmit}>
-        <div className="Form-InputWrapper">
-          <label className="Label" htmlFor="Their Name">
-            Their Name
-          </label>
-          <input
-            className="Input"
-            name="Their Name"
-            type="text"
-            value={recipientName}
-            onChange={(event) => setRecipientName(event.target.value)}
-          />
-        </div>
-        <div className="Form-InputWrapper">
-          <label className="Label" htmlFor="Their Email">
-            Their Email
-          </label>
-          <input
-            className="Input"
-            name="Their Email"
-            type="text"
-            value={recipientEmail}
-            onChange={(event) => setRecipientEmail(event.target.value)}
-          />
-        </div>
+        <LabeledTextInput
+          name="Their Name"
+          value={recipientName}
+          setValue={setRecipientName}
+        />
+        <LabeledTextInput
+          name="Their Email"
+          value={recipientEmail}
+          setValue={setRecipientEmail}
+        />
         <div className="Form-InputWrapper">
           <div className="Label">Pick a Holiday</div>
           <button className="Button" type="button">
             Choose
           </button>
         </div>
-        <div className="Form-InputWrapper">
-          <label className="Label" htmlFor="Your Name">
-            Your Name
+        <LabeledTextInput
+          name="Your Name"
+          value={senderName}
+          setValue={setSenderName}
+        />
+        <LabeledTextInput
+          name="Your Email"
+          value={senderEmail}
+          setValue={setSenderEmail}
+        />
+        <div>
+          <label htmlFor="exampleRef">
+            Uncontrolled Input Example (using useRef())
           </label>
-          <input
-            className="Input"
-            name="Your Name"
-            type="text"
-            value={senderName}
-            onChange={(event) => setSenderName(event.target.value)}
-          />
-        </div>
-        <div className="Form-InputWrapper">
-          <label className="Label" htmlFor="Your Email">
-            Your Email
-          </label>
-          <input
-            className="Input"
-            name="Your Email"
-            type="text"
-            value={senderEmail}
-            onChange={(event) => setSenderEmail(event.target.value)}
-          />
+          <input name="exampleRef" type="text" ref={exampleRef} />
         </div>
         <button type="submit" className="Button">
           Submit
