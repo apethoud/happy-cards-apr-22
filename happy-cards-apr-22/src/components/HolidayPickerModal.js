d5decade3aa992e9eee497d05ec950aa8d5b88fc
Andrew Pethoud <andrewpethoud@gmail.com>
2022-04-25 19:26:43 -0700

    fetch data by timeframe

--- happy-cards-apr-22/src/components/HolidayPickerModal.js
+++ happy-cards-apr-22/src/components/HolidayPickerModal.js
@@ -1,7 +1,9 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import ReactModal from "react-modal";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faXmark } from "@fortawesome/free-solid-svg-icons";
+
+const API_KEY = process.env.REACT_APP_HOLIDAYS_API_KEY;
 
 const modalStyles = {
   overlay: {
@@ -19,6 +21,26 @@ const modalStyles = {
 };
 
 export default function HolidayPickerModal({ isModalOpen, setIsModalOpen }) {
+  const [selectedTimeframe, setSelectedTimeframe] = useState("year");
+  const [holidayList, setHolidayList] = useState([]);
+
+  useEffect(() => {
+    async function getHolidays() {
+      let params;
+      if (selectedTimeframe === "year") {
+        params = "&year=2022";
+      } else if (selectedTimeframe === "month") {
+        params = "&year=2022&month=4";
+      }
+      const url = `https://holidays.abstractapi.com/v1/?api_key=${API_KEY}&country=US${params}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      setHolidayList(data);
+    }
+    getHolidays();
+  }, [selectedTimeframe]);
+
   useEffect(() => {
     ReactModal.setAppElement("body");
   }, []);
@@ -34,6 +56,7 @@ export default function HolidayPickerModal({ isModalOpen, setIsModalOpen }) {
           <FontAwesomeIcon icon={faXmark} size="lg" />
         </div>
       </div>
+      <div>{holidayList.length}</div>
     </ReactModal>
   );
 }
