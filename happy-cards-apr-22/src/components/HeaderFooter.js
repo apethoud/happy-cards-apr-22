a8334993dc78a5d2fd525bfb02ca049cd511a805
Andrew Pethoud <andrewpethoud@gmail.com>
2022-04-27 20:27:30 -0700

    add theme context

--- happy-cards-apr-22/src/components/HeaderFooter.js
+++ happy-cards-apr-22/src/components/HeaderFooter.js
@@ -1,9 +1,14 @@
+import { useContext } from "react";
+import { ThemeContext } from "../contexts/ThemeContext";
+
 export default function HeaderFooter(props) {
-  console.log("props is: ", props);
+  const { theme, toggleTheme } = useContext(ThemeContext);
   return (
     <div>
       <div className="HeaderFooterColorBar" />
       <div className="Logo">HappyCards</div>
+      <button onClick={() => toggleTheme()}>Toggle Theme</button>
+      <div>Current Theme: {theme}</div>
       {props.children}
       <div className="Copyright">&copy; 2022 Andrew Pethoud</div>
     </div>
