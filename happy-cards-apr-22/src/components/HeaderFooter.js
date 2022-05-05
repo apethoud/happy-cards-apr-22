6876b96fc874774350e778529a67af7abdd7de8a
Andrew Pethoud <andrewpethoud@gmail.com>
2022-05-04 17:16:08 -0700

    stubbed out nav links

--- happy-cards-apr-22/src/components/HeaderFooter.js
+++ happy-cards-apr-22/src/components/HeaderFooter.js
@@ -6,10 +6,23 @@ export default function HeaderFooter(props) {
   return (
     <div>
       <div className="HeaderFooterColorBar" />
-      <div className="Logo">HappyCards</div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          padding: 10,
+        }}
+      >
+        <div className="Logo">HappyCards</div>
+        <div style={{ display: "flex" }}>
+          <div className="Link">Home</div>
+          <div className="Link">Make a Card</div>
+          <div className="Link">Login</div>
+        </div>
+      </div>
       <div style={{ padding: 40 }}>
-        <button onClick={() => toggleTheme()}>Toggle Theme</button>
-        <div>Current Theme: {theme}</div>
+        {/* <button onClick={() => toggleTheme()}>Toggle Theme</button>
+        <div>Current Theme: {theme}</div> */}
         {props.children}
       </div>
       <div className="Copyright">&copy; 2022 Andrew Pethoud</div>
